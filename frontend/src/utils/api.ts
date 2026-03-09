cab19d2c7b76dd8dbd448f4f131b74fc3dba3c8a
anwarrali <anwar.roumanen@students.alquds.edu>
2026-03-09 02:20:13 +0200

    2nd commit

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -8,7 +8,8 @@
 
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const BASE_URL =
+  import.meta.env.VITE_API_URL || "https://anly.onrender.com/api";
 
 export const api = axios.create({
   baseURL: BASE_URL,
